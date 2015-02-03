a035ad0eb0e74d4bca9a677e78bb02239999bb4b
Marian Sievers <marian.sievers@gmail.com>
2015-02-03 19:37:34 +0100

    Edit 'docker run' command, add alias to easy ssh into nodejs-container

--- provision.sh
+++ provision.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-echo "----------------------------------------\n"
-echo "====== Starting the provisioning ======\n"
-echo "----------------------------------------\n"
+echo "---------------------------------------"
+echo "====== Starting the provisioning ======"
+echo "---------------------------------------"
 
 # build docker container
 docker build -t kommunalcrowd/node-dev:0.1 /srv/docker/nodejs
 
 # run docker container
-docker run -i -t --rm -p 3000:3000 -v /srv/source:/src kommunalcrowd/node-dev:0.1
+docker run -d -t -p 3000:3000 -v /srv/source:/src --name="nodejs-dev" kommunalcrowd/node-dev:0.1
+
+alias nodessh="alias docker exec -i -t nodejs-dev bash"
+
+echo "Type 'nodessh' in CoreOS to SSH into container"
