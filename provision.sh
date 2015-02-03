4290f5bbb80c521b9def7803fc8b90079cca2900
Marian Sievers <marian.sievers@gmail.com>
2015-02-03 23:01:00 +0100

    Add copy of bashrc with alias for accessing container

--- provision.sh
+++ provision.sh
@@ -8,8 +8,13 @@ echo "---------------------------------------"
 docker build -t kommunalcrowd/node-dev:0.1 /srv/docker/nodejs
 
 # run docker container
-docker run -d -t -p 3000:3000 -v /srv/source:/src --name="nodejs-dev" kommunalcrowd/node-dev:0.1
+docker run -d -t -p 1337:1337 -v /srv/source:/src --name="nodejs-dev" kommunalcrowd/node-dev:0.1
 
-alias nodessh="alias docker exec -i -t nodejs-dev bash"
+# delete symlink to .bashrc and make a real copy
+rm /home/core/.bashrc
+cp /usr/share/skel/.bashrc /home/core/.bashrc
+
+# add alias for sshing into container
+echo "alias nodessh='docker exec -i -t nodejs-dev bash'" >> /home/core/.bashrc
 
 echo "Type 'nodessh' in CoreOS to SSH into container"
