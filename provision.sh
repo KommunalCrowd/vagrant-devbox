a4be97ffe4e6d6c341a14454566b527c4f9ad23a
Marian Sievers <marian.sievers@gmail.com>
2015-02-08 19:40:55 +0100

    Remove building of docker image of provision. Change image from nodejs to iojs

--- provision.sh
+++ provision.sh
@@ -4,17 +4,14 @@ echo "---------------------------------------"
 echo "====== Starting the provisioning ======"
 echo "---------------------------------------"
 
-# build docker container
-docker build -t kommunalcrowd/node-dev:0.1 /srv/docker/nodejs
-
 # run docker container
-docker run -d -t -p 1337:1337 -v /srv/source:/src --name="nodejs-dev" kommunalcrowd/node-dev:0.1
+docker run -d -t -p 1337:1337 -v /srv/source:/src --name="iojs-dev" kommunalcrowd/iojs-dev:0.2
 
 # delete symlink to .bashrc and make a real copy
 rm /home/core/.bashrc
 cp /usr/share/skel/.bashrc /home/core/.bashrc
 
 # add alias for sshing into container
-echo "alias nodessh='docker exec -i -t nodejs-dev bash'" >> /home/core/.bashrc
+echo "alias nodessh='docker exec -i -t iojs-dev bash'" >> /home/core/.bashrc
 
 echo "Type 'nodessh' in CoreOS to SSH into container"
