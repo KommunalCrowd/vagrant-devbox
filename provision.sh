58bdb8a060d17e1b7d1fdd77fe0766122f7497cd
Marian Sievers <marian.sievers@gmail.com>
2015-02-12 19:35:59 +0100

    Add command to start docker to provisioning

--- provision.sh
+++ provision.sh
@@ -12,6 +12,7 @@ yum -y install curl git
 
 # install docker
 yum -y install docker && yum clean all
+sudo systemctl start docker
 
 # install fig
 curl -L https://github.com/docker/fig/releases/download/1.0.1/fig-`uname -s`-`uname -m` > /usr/local/bin/fig; chmod +x /usr/local/bin/fig
